b5fc3c4572c9bc66dce66fbd63309f7e18307959
임성한 <sunghanim0301@gmail.com>
2022-05-28 02:09:52 +0900

    v1

--- pybo/static/main.js
+++ pybo/static/main.js
@@ -1,20 +1,36 @@
 'use strict';
 {
-    const words = [
-        'apple',
-        'sky',
-        'blue',
-        'middle',
-        'set',
-    ]
+    let words = [
 
+    ];
+    wordSetting();
+    function wordSetting() {
+        $.ajax({
+                type: "GET",
+                url: "/quiz/start",
+                data: {},
+                success: function (response) {
+                    let receiveWords = response['word'];
+                    for (let i = 0; i < receiveWords.length; i++) {
+                        words.push(receiveWords[i]);
+                    }
+                    // console.log(words);
+                }
+            })
+    }
+
+    setTimeout(() => {
+    // console.log(words);
     let word = words[Math.floor(Math.random() * words.length)];
     let loc = 0;
     let score = 0;
     let miss = 0;
-    const timeLimit = 3 * 1000; // millisecond
+    const timeLimit = 40 * 1000; // millisecond
     let startTime;
     let isPlaying = false;
+    let exam = 0
+
+    let myRank = true;
 
     const target = document.getElementById('__target');
     const scoreLabel = document.getElementById('__score');
@@ -22,6 +38,82 @@
     const timerLabel = document.getElementById('__timer');
 
     // target.textContent = word;
+
+
+    const pythonWords = $("#__words");
+    pythonWords.hide();
+    const ranking_box = document.getElementById("__words-box");
+    ranking_box.innerText = "랭킹 확인";
+    let ranking_bool = false;
+
+    ranking_box.onclick = function() {Ranking()};
+    document.getElementById("__myRanking").onclick = function() {MyRanking()}
+    function Ranking(){
+            //     if (!ranking_bool) {
+            //         $("#__words").show();
+            //         ranking_box.innerText = "랭킹 박스 닫기";
+            //         ranking_bool = true;
+            //     } else {
+            //         $("#__words").hide();
+            //         $("#__words-box").innerText = "랭킹 박스 열기";
+            //         ranking_bool = false;
+            //     }
+            $.ajax({
+                type: "GET",
+                url: "/quiz/rank",
+                data: {myRank:11},
+                success: function (response) {
+
+                    let message = ''
+                    console.log(response['rank'])
+                    for (let i = 0; i < response['rank'].length; i++) {
+                        for (let j = 0; j < response['rank'][i].length; j++) {
+                            response['rank'][i][0] /= 1000
+                            response['rank'][i][0] = (40 - response['rank'][i][0]).toFixed(3);
+                            message += response['rank'][i][j]
+                            message += '\t\t'
+                        }
+                        message += '\n';
+                    }
+                    console.log(message);
+                    alert('클리어 타임     오타     단어 수        유저 닉네임      클리어한 시간 \n' + message);
+                    //window.location.reload();
+                }
+            })
+
+             }
+
+     function MyRanking(){
+            $.ajax({
+                type: "POST",
+                url: "/quiz/rank",
+                data: {myRank:11},
+                success: function (response) {
+
+                    let message = ''
+                    console.log(response['rank'])
+                    for (let i = 0; i < response['rank'].length; i++) {
+                        for (let j = 0; j < response['rank'][i].length; j++) {
+                            response['rank'][i][0] /= 1000
+                            response['rank'][i][0] = (40 - response['rank'][i][0]).toFixed(3);
+                            message += response['rank'][i][j]
+                            message += '\t\t'
+                        }
+                        message += '\n';
+                    }
+                    console.log(message);
+                    alert('클리어 타임     오타     단어 수        유저 닉네임      클리어한 시간 \n' + message);
+                    //window.location.reload();
+                }
+            })
+
+             }
+
+    document.getElementById('__desc').onclick = function() {
+        alert('타이핑 게임입니다.\n' +
+            '40초 안에 5개 단어를 타이핑하면 됩니다.\n' +
+            '로그인 후, 게임을 클리어하면 기록이 저장됩니다.');
+    }
 
     function updateTarget() {
         let placeholder = '';
@@ -36,7 +128,42 @@
         timerLabel.textContent = (timeLeft/1000).toFixed(2);
 
         const timeOutId = setTimeout(() => {
-           updateTimer();
+            if (exam >= 5) {
+                clearTimeout(timeOutId);
+                alert('Game Clear!');
+
+                // 코드추가 5/25 ajax
+                $(document).ready(function () {
+                    let log = $('#__login').is(':visible');
+                    console.log(log);
+                    if (!log) {
+                        saveData();
+                    }
+
+                });
+
+        function saveData() {
+            let timeScore = timeLeft
+            let wordScore = score
+            let missScore = miss
+            $.ajax({
+                type: "POST",
+                url: "/quiz/save",
+                data: {timeScore_give:timeScore, wordScore_give: wordScore, missScore_give:missScore},
+                success: function (response) {
+                    alert(response["msg"]);
+                    window.location.reload();
+                }
+            })
+        }
+                // 추가코드 종료 5/25 ajax
+
+
+
+            } else {
+                updateTimer();
+            }
+
         }, 10);
 
         if (timeLeft < 0) {
@@ -45,14 +172,14 @@
 
             clearTimeout(timeOutId);
             timerLabel.textContent = '0.00';
-            setTimeout(() => {
-                alert('Game Over');
-            }, 100)
 
         }
     }
 
-    window.addEventListener('click', () =>{
+    document.getElementById("__target").addEventListener('click', gameStart);
+
+
+    function gameStart() {
         if (isPlaying === true) {
             return;
         }
@@ -61,7 +188,7 @@
         target.textContent = word;
         startTime = Date.now();
         updateTimer();
-    })
+    }
 
     window.addEventListener('keydown', (e)=> {
         if (isPlaying !== true) {
@@ -73,6 +200,8 @@
             if (loc === word.length) {
                 word = words[Math.floor(Math.random() * words.length)];
                 loc = 0;
+                exam++;
+                console.log(exam);
             }
             updateTarget();
             score++;
@@ -82,4 +211,5 @@
             missLabel.textContent = miss;
         }
     });
+    }, 50)
 }
--- pybo/views/quiz_views.py
+++ pybo/views/quiz_views.py
@@ -1,12 +1,67 @@
+import json, datetime
 from datetime import datetime
 
-from flask import Blueprint, url_for, request, render_template
+from flask import Blueprint, url_for, request, render_template, jsonify, g
 from werkzeug.utils import redirect
 
-from pybo import db
+from .. import db
+from pybo.models import Words, Quiz
+
 
 bp = Blueprint('quiz', __name__, url_prefix='/quiz')
+
 
 @bp.route('/')
 def create():
     return render_template('quiz/index.html')
+
+@bp.route('/save/', methods=('POST',))
+def save():
+    timeScore_receive = int(request.form['timeScore_give'])
+    wordScore_receive = int(request.form['wordScore_give'])
+    missScore_receive = int(request.form['missScore_give'])
+    print(timeScore_receive ,wordScore_receive, missScore_receive)
+
+
+    quiz = Quiz(score=timeScore_receive, miss=missScore_receive, word_count=wordScore_receive, username=g.user.username, create_date=datetime.now())
+    db.session.add(quiz)
+    db.session.commit()
+    return jsonify({'msg': 'db 저장 완료'})
+
+@bp.route('/start/', methods=('GET',))
+def start():
+    word_list = Words.query.order_by(Words.id.asc()).all()
+    word = []
+    for i in word_list:
+        word.append(i.word)
+    # jsonWord = dict(zip(range(1, len(word) + 1), word))
+    jsonWord = {'word': word}
+    return jsonify(jsonWord)
+
+@bp.route('/rank/', methods=('GET', 'POST'))
+def rank():
+    if request.method == 'POST':
+        quiz_rank = Quiz.query.filter(Quiz.username == g.user.username).order_by(Quiz.score.desc()).limit(5)
+    else:
+        quiz_rank = Quiz.query.order_by(Quiz.score.desc()).limit(5)
+
+    print(quiz_rank)
+    rank = [[], [], [], [], []]
+    k = 0
+
+    for i in quiz_rank:
+        for j in range(k, k+1):
+            rank[j].append(i.score)
+            rank[j].append(i.miss)
+            rank[j].append(i.word_count)
+            rank[j].append(i.username)
+
+            clearTime = i.create_date.strftime('%Y/%m/%d %H:%M')
+            rank[j].append(clearTime)
+
+        k+=1
+    for i in rank:
+        print(i)
+
+    jsonRank = {'rank': rank}
+    return jsonify(jsonRank)
